e408896ea5b42611b0f12f346a877d99ef07c708
Adam D <adamdebrouwere@hotmail.com>
2024-07-28 17:53:58 +0000

    feat: psql query to get info for display message

--- element.sh
+++ element.sh
@@ -6,4 +6,12 @@ if [[ -z "$1" ]]
 then
   echo "Please provide an element as an argument."
   exit
+
+else
+
+  if [[ $1 = ^[0-9]+$ ]]
+  then
+    GET_ELEMENT_INFO=$($ PSQL "SELECT properties.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM properties FULL JOIN types USING (type_id) FULL JOIN elements USING (atomic_number);")
+  fi
 fi
+
