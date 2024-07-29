3e2c834de99d92a662d790cded417cff4d4f6e8c
Adam D <adamdebrouwere@hotmail.com>
2024-07-29 16:43:16 +0000

    feat: display output in a sentence

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #periodic table elements search program
 
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t -c"
 
 if [[ -z "$1" ]] 
 then
@@ -23,5 +23,9 @@ then
   echo "I could not find that element in the database."
 
 else
-  echo "$GET_ELEMENT_INFO"
+   
+  echo "$GET_ELEMENT_INFO" | while read ATOMIC_NUMBER _ NAME _ SYMBOL _ TYPE _ ATOMIC_MASS _ MELTING_POINT _ BOILING_POINT
+  do
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
 fi
