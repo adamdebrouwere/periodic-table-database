5242fe660d735bb36c40cc5bc2a9fdbf9dd24eaa
Adam D <adamdebrouwere@hotmail.com>
2024-07-29 15:36:39 +0000

    feat: name or symbol user input functionality

--- element.sh
+++ element.sh
@@ -6,12 +6,15 @@ if [[ -z "$1" ]]
 then
   echo "Please provide an element as an argument."
   exit
-
-else
-
-  if [[ $1 = ^[0-9]+$ ]]
-  then
-    GET_ELEMENT_INFO=$($ PSQL "SELECT properties.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM properties FULL JOIN types USING (type_id) FULL JOIN elements USING (atomic_number);")
-  fi
 fi
 
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  GET_ELEMENT_INFO=$($PSQL "SELECT properties.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM properties FULL JOIN types USING (type_id) FULL JOIN elements USING (atomic_number) WHERE atomic_number = '$1';")
+  echo "$GET_ELEMENT_INFO"
+
+elif [[ $1 =~ ^[a-zA-Z]+$ ]]
+then
+  GET_ELEMENT_INFO=$($PSQL "SELECT properties.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM properties FULL JOIN types USING (type_id) FULL JOIN elements USING (atomic_number) WHERE symbol = '$1' OR name = '$1';")
+  echo "$GET_ELEMENT_INFO"
+fi
