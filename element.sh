b69febdc2f576c2ce8e6730459fbdae53b672875
Adam D <adamdebrouwere@hotmail.com>
2024-07-29 16:00:29 +0000

    feat: unknown element error message

--- element.sh
+++ element.sh
@@ -11,10 +11,17 @@ fi
 if [[ $1 =~ ^[0-9]+$ ]]
 then
   GET_ELEMENT_INFO=$($PSQL "SELECT properties.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM properties FULL JOIN types USING (type_id) FULL JOIN elements USING (atomic_number) WHERE atomic_number = '$1';")
-  echo "$GET_ELEMENT_INFO"
-
+ 
 elif [[ $1 =~ ^[a-zA-Z]+$ ]]
 then
   GET_ELEMENT_INFO=$($PSQL "SELECT properties.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM properties FULL JOIN types USING (type_id) FULL JOIN elements USING (atomic_number) WHERE symbol = '$1' OR name = '$1';")
+  
+fi
+
+if [[ -z "$GET_ELEMENT_INFO" ]]
+then
+  echo "I could not find that element in the database."
+
+else
   echo "$GET_ELEMENT_INFO"
 fi
